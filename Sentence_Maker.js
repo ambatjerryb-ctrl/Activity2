2bf31ce29cad83f673931148336f6729068d1a2b
ambatjerryb-ctrl <ambat.jerryb@spamast.edu.ph>
2026-02-15 11:51:59 +0800

    first commit

---
+++ Sentence_Maker.js
@@ -0,0 +1,18 @@
+
+adjective = "brave";
+noun = "dragon";
+verb = "sleeping";
+place = "forest";
+adjective2 = "fiery";
+noun2 = "apples";
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb};`;
+console.log("First story: " + firstStory);
+
+adjective = "gentle";
+noun = "giant";
+verb = "dancing";
+place = "village";
+adjective2 = "sparkling";
+noun2 = "bananas";
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb};`;
+console.log("Second story: " + secondStory);
